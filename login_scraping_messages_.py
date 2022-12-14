05fb9d6564aaba3758e96eb7b776a245090a65fb
Nora Pauelsen <nora.pauelsen@gmail.com>
2022-12-14 15:16:06 +0100

    added: Printing sent message and time, date

--- login_scraping_messages_.py
+++ login_scraping_messages_.py
@@ -44,25 +44,37 @@ for tab in tabs:
         list_refs = div.find_elements(by="xpath", value='.//a')  # all conversations
         for index in range(len(list_refs)):  # for all matches conversations
             print("we have", len(list_refs), "tinder chats")
+            print("____START: CHAT WITH MATCH", index, "____")
             driver.execute_script("arguments[0].click();", list_refs[index])  # open chat
 
             # All messages
             xpath_messages = '//div[starts-with(@class, "msgHelper")]'
             messages_helpers = driver.find_elements(By.XPATH, xpath_messages)
 
+            #mMESSAGES RECEIVED
             # messages received:Content
             xpath_helpers_received = '//div[contains(@class, "msgHelper")]//div[contains(@class, "ds-text-chat-bubble-receive")]//span'
             helpers_received = driver.find_elements(By.XPATH, xpath_helpers_received)
-            for message in helpers_received:
-                print("message is", message.text)
 
+            for message_received in helpers_received:
+                print("received message is", message_received.text)
             # messages received:Time
             xpath_helpers_received_time = '//div[contains(@class, "Ta(start)")] //div[contains(@class, "msgHelper")]//time'
             helpers_received_time = driver.find_elements(By.XPATH, xpath_helpers_received_time)
             for time_message in helpers_received_time:
-                print("time of message is", time_message.get_attribute("datetime"))
+                print("time of received message is", time_message.get_attribute("datetime"))
 
-            # xpath_receive_content = '//div[contains(@class, "msgHelper")]//div[contains(@class, "ds-text-chat-bubble-receive")]'
+            #MESSAGES SENT
+            #Content
+            xpath_helpers_sent = '//div[contains(@class, "msgHelper")]//div[contains(@class, "ds-text-chat-bubble-sent")]//span'
+            helpers_sent = driver.find_elements(By.XPATH, xpath_helpers_sent)
+            for message_sent in helpers_sent:
+                print("sent message is", message_sent.text)
+            #Time
+            xpath_helpers_sent_time = '//div[contains(@class, "Ta(e)")] //div[contains(@class, "msgHelper")]//time'
+            helpers_sent_time = driver.find_elements(By.XPATH, xpath_helpers_sent_time)
+            for time_message_sent in helpers_sent_time:
+                print("time of sent message is", time_message_sent.get_attribute("datetime"))
 
             time.sleep(5)
-            print("chat", index, "was clicked")
+            print("END: CHAT WITH MATCH", index)
